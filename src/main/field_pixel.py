f009f039b39488e689dfb2ef9b63354934a6fb45
Bryce Turner <74370313+brycecturner@users.noreply.github.com>
2022-11-21 17:52:28 -0500

    Dev bryce football field implementation (#5)
    * find_center_of_square_containing
    
    Function to find the square containing a given point. To be used to determine where to draw the "pocket' based on the x,y of the football in frame 1
    
    * play_pocket and field_utils
    
    Created a play to keep track of the utility functions for the football_field class
    
    Began constructing the play_pocket class, which will extend "football_field"
    
    * Update play_pocket.py
    
    added logic to define pocket width and depth
    
    * play_pocket and tests
    
    untested play_pocket class and accompanying tests.  Unable to run the tests on my laptop for some reason. Will try to check when I'm home.
    
    * play_pocket unit tests fixed
    
    fixing a few typos/ other errors in the play_pocket unit tests
    
    * Delete __init__.py
    
    * fixed play_pocket
    
    A few more typoes. Change offence to offense.
    
    * find_center_of_square_containing
    
    testing some floating point errors for this function. Seems ok down to side_length=0.00005
    
    * play_pocket final
    
    Had to rework the play_pocket mechanics so that I can include the entirety of the pocket (rather than chopping off the parts that didn't neatly fall into the determined squares).
    
    * prepping for PR
    
    changed the file names to conform to the new naming schema (pixel).
    Edited the set_field_pixels modulo check to avoid floating-point issues if inputs are not integers
    
    * removing weird "HEADS"
    
    the git suggested changes somehow got committed in the actual file. Need to manually clean these up
    
    * Update play_pocket.py
    
    forgot to change "square" to "pixel" in one instance
    
    * resolving merge conflicts

--- src/main/field_pixel.py
+++ src/main/field_pixel.py
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 5ef4ecb17b8d764a53b9887d6c7311d4106bf3d7
 class field_pixel:
     def __init__(self, center, pixel_length, units="yds"):
         self.center=center
         self.pixel_length=pixel_length
         self.units=units
         
-<<<<<<< HEAD
     def set_pixel_corners(self):
         # I'm not sure if we need this right now, but it might be useful later for charting
         """  
-        Calulates the perimeter of the pixel 
-=======
-    def set_square_corners(self):
-        # I'm not sure if we need this right now, but it might be useful later for charting
-        """  
         Calulates the perimeter of the square 
->>>>>>> 5ef4ecb17b8d764a53b9887d6c7311d4106bf3d7
             Returns the points as tuples in the following order:
                (x_0, y_0)     (x_1, y_0) 
                
@@ -31,11 +20,6 @@ class field_pixel:
         y_1=self.center[1]+(self.pixel_length/2)
         y_0=self.center[1]-(self.pixel_length/2)
         
-<<<<<<< HEAD
-        self.pixel_corners=[(x_0, y_1), (x_1, y_1), 
-                             (x_0, y_0), (x_1, y_0)]
-=======
         self.square_corners=[(x_0, y_1), (x_1, y_1), 
                              (x_0, y_0), (x_1, y_0)]
     
->>>>>>> 5ef4ecb17b8d764a53b9887d6c7311d4106bf3d7
--- src/test/test_field_pixel.py
+++ src/test/test_field_pixel.py
@@ -5,7 +5,7 @@ sys.path.insert(0, "..\\main")
 from field_pixel import field_pixel
 
 
-<<<<<<< HEAD
+
 working_pixel=field_pixel((10, 10), 1)
 
 def test_assignment():
@@ -20,22 +20,6 @@ def test_correct_pixel_sides():
     working_pixel.set_pixel_corners()
 
     assert working_pixel.pixel_corners == [(9.5, 10.5), (10.5, 10.5), 
-=======
-working_square=field_pixel((10, 10), 1)
-
-def test_assignment():
-    
-    assert working_square.center==(10, 10)
-    assert type(working_square.center) is tuple
-    assert working_square.center[0]==10
-    assert working_square.pixel_length==1
-    assert working_square.pixel_length/2==0.5
-
-def test_correct_square_sides():
-    working_square.set_square_corners()
-
-    assert working_square.square_corners == [(9.5, 10.5), (10.5, 10.5), 
->>>>>>> 5ef4ecb17b8d764a53b9887d6c7311d4106bf3d7
                                           (9.5, 9.5),  (10.5, 9.5)] 
 
 
