89907fddecef4af81d908a8db9af71293dab43d7
Miguel Cacho Soblechero <miguelcachosoblechero@gmail.com>
2022-12-14 17:47:50 -0500

    Add gaussian player influence

--- src/utils/player_influence.py
+++ src/utils/player_influence.py
@@ -29,6 +29,29 @@ from definitions.fouls_def import ignore_fouls
 from src.utils.viz_functions import drawPitch, drawPocket, animatePlay_Generic, animateScores
 from src.utils.play_preprocessing import extractPlay, preprocessPlay_refBallInit, preprocessPlay_refQB, preprocessPlay_refQB_NFrames
 
+def gaussian_player_influence_score(player_x, player_y, pixel_x, pixel_y, config):
+
+    '''
+    This function calculates the player influence using the gaussian function
+    The thresholding function is the numpy implementation of MAX
+    :param player_x: Player X coordinates 
+    :param player_y: Player Y coordinates 
+    :param pixel_x: Mesh values on the pocket X coordinates 
+    :param pixel_y: Mesh values on the pocket Y coordinates 
+    :param config: Run Parameters 
+    :return: Player influence value
+    '''
+
+    # Determine the relative position of the pixel from the player
+    X = player_x - pixel_x
+    Y = player_y - pixel_y
+
+    # Calculate Gaussian Distribution
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.exp(-( (R-config['gaus_mu'])**2 / ( 2.0 * config['gaus_sigma']**2 ) ) )
+    
+    return Z
+
 def extract_play_players_influence(team_def, infl_funct, config):
 
     '''
@@ -51,11 +74,11 @@ def extract_play_players_influence(team_def, infl_funct, config):
     array_dimension = x.shape
     scores = np.empty((num_frames, array_dimension[0], array_dimension[1]))
     for frame in range(num_frames):
-        scores[frame, :, :] = calculate_grid_score(x, y, frame+1, team_def=team_def, infl_funct=infl_funct)
+        scores[frame, :, :] = calculate_grid_score(x, y, frame+1, team_def=team_def, infl_funct=infl_funct, config=config)
 
     return scores
 
-def calculate_grid_score(pixel_x, pixel_y, frameId, team_def, infl_funct):
+def calculate_grid_score(pixel_x, pixel_y, frameId, team_def, infl_funct, config):
 
     '''
     This function calculates the grid scope for an specific frame, given a specific influence function
@@ -65,6 +88,7 @@ def calculate_grid_score(pixel_x, pixel_y, frameId, team_def, infl_funct):
     :param frameId: Value of the current frame
     :param team_def: DataFrame with defensive team information
     :param infl_funct: Influence function to use
+    :param config: Run Parameters 
     :return: Mesh values with score results for frame frameId
     '''
  
@@ -80,9 +104,9 @@ def calculate_grid_score(pixel_x, pixel_y, frameId, team_def, infl_funct):
             player_x=def_player.x,
             player_y=def_player.y,
             pixel_x=pixel_x,
-            pixel_y=pixel_y
+            pixel_y=pixel_y,
+            config=config
         )
 
     # At the end of this calculation, only maintain the maximum one
     return frame_scores.max(axis=2)
-    
